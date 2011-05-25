f3e84f2196287638a9fcbe285446affa9a42a929
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-05-25 15:46:03 -0700

    indentation problems

--- shift.c
+++ shift.c
@@ -1,7 +1,7 @@
-/* -*- Mode: C; tab-width: 4; c-basic-offset: 4 -*- */
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 8 -*- */
 /*!
   @todo get info on CPUs, cores, and frequencies from system
- */
+*/
 
 #include <assert.h>
 #include <stdio.h>
@@ -17,16 +17,16 @@ static int current_freq=0;
 int
 shift(int freq_idx){
 #ifdef BLR_USE_SHIFT
-        char *cpufreq_filename[] = {
+	char *cpufreq_filename[] = {
 		"/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed",
 		"/sys/devices/system/cpu/cpu1/cpufreq/scaling_setspeed",
 		"/sys/devices/system/cpu/cpu2/cpufreq/scaling_setspeed",
 		"/sys/devices/system/cpu/cpu3/cpufreq/scaling_setspeed"};
-        static char *freq_str[]=
-                {"1800000", "1600000", "1400000", "1200000", "1000000"};
-        FILE *sfp;
+	static char *freq_str[]=
+		{"1800000", "1600000", "1400000", "1200000", "1000000"};
+	FILE *sfp;
 #endif
-        static int prev_freq_idx[4]={0, 0, 0, 0};
+	static int prev_freq_idx[4]={0, 0, 0, 0};
 	static int cpuid = -1;
 	static int shift_initialized=0;
 	int temp_cpuid;
@@ -37,7 +37,7 @@ shift(int freq_idx){
 		sfp = fopen(cpufreq_filename[ 1 ], "w");
 		if(!sfp){
 			fprintf(stderr, "!!! cpufreq_filename[%d]=%s does not exist.  Bye!\n", 
-					cpuid, cpufreq_filename[cpuid]);
+				cpuid, cpufreq_filename[cpuid]);
 		}
 		assert(sfp);
 		fprintf(sfp, freq_str[ 0 ]);
@@ -58,9 +58,9 @@ shift(int freq_idx){
 	temp_cpuid = get_cpuid();
 	assert( temp_cpuid == cpuid );
 
-        if( freq_idx == prev_freq_idx[ cpuid ] ){
-                return freq_idx;
-        }
+	if( freq_idx == prev_freq_idx[ cpuid ] ){
+		return freq_idx;
+	}
 	prev_freq_idx[ cpuid ] = freq_idx;
 	
 	assert( (freq_idx >= 0) && (freq_idx <= 4) );
@@ -70,7 +70,7 @@ shift(int freq_idx){
 	sfp = fopen(cpufreq_filename[ cpuid ], "w");
 	if(!sfp){
 		fprintf(stderr, "!!! cpufreq_filename[%d]=%s does not exist.  Bye!\n", 
-				cpuid, cpufreq_filename[cpuid]);
+			cpuid, cpufreq_filename[cpuid]);
 	}
 	assert(sfp);
 	fprintf(sfp, freq_str[ freq_idx ]);
