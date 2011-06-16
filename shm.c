933cf5c7bb3f22214dbc18f4f1f56451b961225e
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-06-15 18:44:41 -0700

    warnings

--- shm.c
+++ shm.c
@@ -1,14 +1,18 @@
+#include <assert.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <semaphore.h>
+#include <stdio.h>
 
 static int shm_fd = -1;
 
 int setup_shm(char **argv, const char *hostname){
   char name[NAME_MAX];
-  snprintf(name, NAME_MAX, "/%s.%s");
+  assert(argv && argv[0] && hostname);
+  snprintf(name, NAME_MAX, "/%s.%s", argv[0], hostname);
   shm_fd = shm_open(name, O_RDWR | O_CREAT, S_IRWXU);
   if(shm_fd < 0){
     perror("shm_open");
