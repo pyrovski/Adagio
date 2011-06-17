c205ba8e8bbd0be424a3d352dfe41cbeca3ac017
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-06-17 16:32:19 -0700

    fix some warnings, introduce others...

--- wpapi.c
+++ wpapi.c
@@ -8,6 +8,17 @@
 #include <errno.h>
 #include "wpapi.h"
 
+static void initialize_papi();
+static int wpapi_library_init(int version);
+static int wpapi_create_eventset (int *eventset);
+static int wpapi_add_events(int eventset, int *eventcodes, int number);
+static int wpapi_add_event(int eventset, int eventcode);
+static int wpapi_read(int EventSet, long_long *values);
+static int wpapi_accum(int EventSet, long_long *values);
+static int wpapi_start(int EventSet);
+static int wpapi_stop(int EventSet, long_long *values);
+static int wpapi_reset(int EventSet);
+
 static int EventSet = PAPI_NULL;
 static long_long inst;
 static void initialize_papi(void);
