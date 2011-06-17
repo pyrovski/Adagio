a377b88e9431b0423469fa8733fc6b5721e7ced7
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-06-17 16:15:59 -0700

    add core-to-socket mapping from mcsup

--- cpuid.c
+++ cpuid.c
@@ -1,11 +1,212 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "cpuid.h"
 
-int
-get_cpuid(void){
+/*-----------------------------------------------------------------------*/
+/* adapted from                                                          */
+/* Multicore/NUMA support library                                        */
+/* Martin Schulz, LLNL, 2008/2009                                        */
+/*-----------------------------------------------------------------------*/
+
+#define MCSUP_OK                 0
+#define MCSUP_WARN_TOOLONG       1
+#define MCSUP_ERR_FILEIO        -1
+#define MCSUP_ERR_IDOUTOFRANGE  -2
+#define MCSUP_ERR_PARSEERROR    -3
+#define MCSUP_ERR_NOMEM         -7
+
+#define MAX_LINE 256
+
+int whitespace(char c)
+{
+  if ((c=='\t') || (c==' ') || (c=='\n') || (c==EOF))
+    return 1;
+  #ifdef AIX
+  if ((int)c==255)
+    return 1;
+  #endif
+  return 0;
+}
+
+int readline(FILE *fd, char *line, int len)
+{
+  char c,lastc;
+  int pos;
+
+  pos=0;
+  lastc=' ';
+  c=' ';
+
+  while ((!feof(fd)) && (c!='\n'))
+    {
+      c=(char)getc(fd);
+
+      if (pos==len-1)
+	{
+	  line[len]=(char) 0;
+	  return MCSUP_WARN_TOOLONG;
+	}
+      else
+	{
+	  if ((!(whitespace(c))) || (!(whitespace(lastc))))
+	    {
+	      if (whitespace(c))
+		line[pos]=' ';
+	      else
+		line[pos]=c;
+	      lastc=c;
+	      pos++;
+	    }
+	}
+    }
+  line[pos]=(char) 0;
+  return MCSUP_OK;
+}
+
+typedef struct mcsup_nodeconfig_d 
+{
+  int sockets;
+  int cores;
+  int cores_per_socket;
+  int *map_core_to_socket;   /* length: cores */
+  int *map_core_to_local;    /* length: cores */
+  int **map_socket_to_core;  /* length: sockets, cores_per_socket */
+} mcsup_nodeconfig_t;
+
+static mcsup_nodeconfig_t mcsup_nodeconfig;
+
+int parse_proc_cpuinfo(mcsup_nodeconfig_t *config)
+{
+  char line[MAX_LINE];
+  FILE *cpuinfo;
+  int core,i,j,err = -1;
+  
+  cpuinfo=fopen("/proc/cpuinfo","r");
+  if (cpuinfo==NULL)
+    return MCSUP_ERR_FILEIO;
+
+  config->cores=0;
+  while (!(feof(cpuinfo)))
+    {
+      err=readline(cpuinfo,line,MAX_LINE);
+      if (err<MCSUP_OK) 
+	return err;
+
+      if (strncmp(line,"processor",9)==0)
+	{
+	  config->cores++;
+	}
+    }
+
+  config->map_core_to_socket=(int*)malloc(config->cores*sizeof(int));
+  config->map_core_to_local=(int*)malloc(config->cores*sizeof(int));
+
+    if ((config->map_core_to_socket==NULL)||
+      (config->map_core_to_local==NULL))
+    return MCSUP_ERR_NOMEM;
+
+  for (i=0; i<config->cores; i++)
+    {
+      config->map_core_to_socket[i]=-1;
+      config->map_core_to_local[i]=-1;
+    }
+
+
+  cpuinfo=freopen("/proc/cpuinfo","r",cpuinfo);
+  if (cpuinfo==NULL)
+    return MCSUP_ERR_FILEIO;
+
+  core=-1;
+
+  while (!(feof(cpuinfo)))
+    {
+      readline(cpuinfo,line,MAX_LINE);
+      if (err<MCSUP_OK) 
+	return err;
+
+      if (strncmp(line,"processor",9)==0)
+	{
+	  core=atoi(strchr(line,':')+2);
+	  if ((core<0) || (core>=config->cores))
+	    return MCSUP_ERR_IDOUTOFRANGE;
+	}
+
+      if (strncmp(line,"physical id",11)==0)
+	{
+	  if (core>=0)
+	    config->map_core_to_socket[core]=atoi(strchr(line,':')+2);
+	  else
+	    return MCSUP_ERR_PARSEERROR;
+	}
+
+      if (strncmp(line,"core id",7)==0)
+	{
+	  if (core>=0)
+	    config->map_core_to_local[core]=atoi(strchr(line,':')+2);
+	  else
+	    return MCSUP_ERR_PARSEERROR;
+	}
+    }
+
+  fclose(cpuinfo);
+
+  config->sockets=0;
+  config->cores_per_socket=0;
+
+  for (i=0; i<config->cores; i++)
+    {
+      if (config->map_core_to_socket[i]+1>config->sockets)
+	config->sockets=config->map_core_to_socket[i]+1;
+      if (config->map_core_to_local[i]+1>config->cores_per_socket)
+	config->cores_per_socket=config->map_core_to_local[i]+1;
+    }
+
+  config->map_socket_to_core=(int**)malloc(sizeof(int*)*config->sockets);
+  for (i=0; i<config->sockets; i++)
+    {
+      config->map_socket_to_core[i]=(int*)malloc(sizeof(int)*config->cores_per_socket);
+      core=0;
+      for (j=0; j<config->cores; j++)
+	{
+	  if (config->map_core_to_socket[j]==i)
+	    {
+	      config->map_socket_to_core[i][core]=j;
+	      core++;
+	    }
+	}
+    } 
+
+#ifdef VERBOSE
+  printf("Found %i cores, %i sockets, %i cores per socket\n",
+	 config->cores,config->sockets,config->cores_per_socket);
+  printf("Mapping of cores to sockets:\n");
+  for (i=0; i<config->cores; i++)
+    {
+      printf("\tCore %2i -> Socket %2i\n",i,config->map_core_to_socket[i]);
+    }
+  printf("Mapping of sockets to cores:\n");
+  for (i=0; i<config->sockets; i++)
+    {
+      printf("\tSocket %2i ->",i);
+      for (j=0; j<config->cores_per_socket; j++)
+	printf(" %i",config->map_socket_to_core[i][j]);
+      printf("\n");
+    }
+#endif
+
+  return MCSUP_OK;
+}
+
+
+int get_cpuid(int *core, int *socket, int *local){
   //Return value should be in the range 0-3.
   int a,b,c,d;
-  int apic_id;
+
+  assert(core);
+  assert(socket);
+  assert(local);
 
 #define cpuid( in, a, b, c, d )					\
   asm ( "cpuid" :						\
@@ -15,10 +216,12 @@ get_cpuid(void){
   //Figure out which core we're on.
   cpuid( 1, a, b, c, d );
   a=a; b=b; c=c; d=d;
-  apic_id = ( b & INITIAL_APIC_ID_BITS ) >> 24;
   
-
+  *core = ( b & INITIAL_APIC_ID_BITS ) >> 24;
+  *socket=mcsup_nodeconfig.map_core_to_socket[*core];
+  *local=mcsup_nodeconfig.map_core_to_local[*core];
+  
 #undef cpuid
 #undef INITAL_APIC_ID_BITS
-  return apic_id;
+  return 0;
 }
