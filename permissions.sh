d79e7ae43e921e23c9c5b9dcb812d08377682745
Peter Bailey <peter.eldrige.bailey@gmail.com>
2011-06-24 10:02:29 -0700

    also chmod governor selector

--- permissions.sh
+++ permissions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod go+rw /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 chmod go+rw /sys/devices/system/cpu/cpu*/cpufreq/scaling_setspeed
 chmod go+rw /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
 chmod go+rw /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
