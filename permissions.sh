d17f6118c574688b85b6014836edbe80cc98a1be
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-08-31 14:17:30 -0700

    split permissions script

--- permissions.sh
+++ permissions.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in `seq 1 16`; do rsh hyperion-fio$i chmod go+rw /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor /sys/devices/system/cpu/cpu*/cpufreq/scaling_setspeed /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq; done
+chmod go+rw /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor /sys/devices/system/cpu/cpu*/cpufreq/scaling_setspeed /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
