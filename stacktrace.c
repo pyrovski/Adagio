c046428eb68db2f09abe1b1f9d8fc2c70760f18d
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-01-18 11:53:06 -0800

    error checking

--- stacktrace.c
+++ stacktrace.c
@@ -1,5 +1,6 @@
 #include "stacktrace.h"
 #include "md5.h"
+#include <stdio.h>
 #define UNW_LOCAL_ONLY
 #include <libunwind.h>
 
@@ -12,16 +13,35 @@ int hash_backtrace(int fid) {
 
 	md5_state_t pms;
 	md5_byte_t digest[16];
+
+	int status;
+
 	md5_init(   &pms );
 
-	unw_getcontext(&uc);
-	unw_init_local(&cursor, &uc);
-	while (unw_step(&cursor) > 0) {
-		unw_get_reg(&cursor, UNW_REG_IP, &ip);
-		unw_get_reg(&cursor, UNW_REG_SP, &sp);
-		md5_append( &pms, (md5_byte_t *)(&ip), sizeof(unw_word_t) );
-		md5_append( &pms, (md5_byte_t *)(&sp), sizeof(unw_word_t) );
+	status = unw_getcontext(&uc);
+	if(status != 0)
+	  fprintf(stderr, "unw_getcontext() error\n");
+
+	status = unw_init_local(&cursor, &uc);
+	if(status != 0)
+	  fprintf(stderr, "unw_getcontext() error\n");
+
+	while ((status = unw_step(&cursor)) > 0) {
+	  status = unw_get_reg(&cursor, UNW_REG_IP, &ip);
+	  if(status != 0)
+	    fprintf(stderr, "unw_getcontext() error\n");
+
+	  status = unw_get_reg(&cursor, UNW_REG_SP, &sp);
+	  if(status != 0)
+	    fprintf(stderr, "unw_getcontext() error\n");
+
+	  md5_append( &pms, (md5_byte_t *)(&ip), sizeof(unw_word_t) );
+	  md5_append( &pms, (md5_byte_t *)(&sp), sizeof(unw_word_t) );
+	  status = 0;
 	}
+	if(status < 0)
+	  fprintf(stderr, "unw_step() error\n");
+
 	md5_append( &pms, (md5_byte_t*)(&fid), sizeof(int) );
 	md5_finish( &pms, digest );
 	return *((int*)digest) & 0x1fff; //8192 entries.
