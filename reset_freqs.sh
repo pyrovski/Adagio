54cad2199f5b4e77875abc85a9aad82603ec0d43
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-02-16 20:24:01 -0800

    turn off rapl clamping

--- reset_freqs.sh
+++ reset_freqs.sh
@@ -3,3 +3,4 @@ cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | cut -d'
 cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | tr -s ' ' | tr ' ' '\n'|sed -e '/^$/d'|tail -n 1|tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq > /dev/null
 cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | cut -d' ' -f1 | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_setspeed > /dev/null
 $HOME/local/bin/turbo off
+$HOME/local/bin/rapl_clamp off
